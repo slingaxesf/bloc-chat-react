b6aec10e13ca288a8e085282546be7187052f313
slingaxesf <43391780+slingaxesf@users.noreply.github.com>
2019-03-18 18:14:56 -0700

    6:15pm

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -15,12 +15,13 @@ class RoomList extends Component {
      this.roomsRef.on('child_added', snapshot => {
      const room = snapshot.val();
      room.key = snapshot.key;
-     
-     this.setState({ rooms: this.state.rooms.concat( room ) })
-       });
-     }
-
+     console.log("before:" + this.state.rooms);
+     this.setState({ rooms: this.state.rooms.concat( room ) });
+     console.log("after: " +  this.state.rooms);
+   });
+}
   render() {
+
     return (
       <span>
         Available Chat Rooms:
