1422a8d3c0bbc4200d34641cc931d13ade79ed20
slingaxesf <43391780+slingaxesf@users.noreply.github.com>
2019-03-20 18:33:14 -0700

    room list working

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -1,39 +1,47 @@
 import React, { Component } from 'react';
 
 class RoomList extends Component {
-   constructor(props) {
-      super(props);
-      this.state = {
-         rooms: []
-     };
-   this.roomsRef = this.props.firebase.database().ref('rooms');
-   }
+  constructor(props) {
+    super(props);
+    this.state = {
+      rooms: []
+    };
+    this.roomsRef = this.props.firebase.database().ref('rooms');
+  }
 
-   componentDidMount() {
-     this.roomsRef.on('child_added', snapshot => {
-     const room = snapshot.val();
-     room.key = snapshot.key;
-     this.setState({ rooms: this.state.rooms.concat( room ) })
+  componentDidMount() {
+    this.roomsRef.on('child_added', snapshot => {
+      const room = snapshot.val();
+      room.key = snapshot.key;
+      this.setState({ rooms: this.state.rooms.concat( room ) })
     });
     console.log(this.state.rooms && this.state.rooms.length && this.state.rooms[0].key);
-   }
-
-   componentWillUnmount(){
-
-
-
-   }
-   render() {
-
-      return (
-          {this.state.rooms.map(room, index) => {
-             return  <li key = {index}>{room.name}</li>
-
-          }
-      );
-
-   }
   }
+
+  componentWillUnmount(){
+
+
+
+  }
+
+  render() {
+    let rooms  = this.state.rooms.map( (room, index) => {
+      return <li key={index}>{room.name}</li>
+    });
+
+    return (
+      <div>
+      {rooms}
+
+      {/*
+      {this.state.rooms.map( (room, index) => {
+        return <li key={index}>{room.name}</li>
+      })}
+      */}
+      </div>
+    );
+  }
+
 } //end class declaration
 
 export default RoomList;
